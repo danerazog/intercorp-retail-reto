86c03123a5a497c8453ee040b7f1a344e7e406c6
Dan Erazo <dan.erazog@gmail.com>
2019-08-25 21:37:56 -0500

    Se agrega configuracion de Swagger

--- src/main/java/com/intercorp/retail/reto/clientes/web/controller/ClienteController.java
+++ src/main/java/com/intercorp/retail/reto/clientes/web/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.intercorp.retail.reto.clientes.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ import com.intercorp.retail.reto.clientes.model.Cliente;
 import com.intercorp.retail.reto.clientes.model.KpiCliente;
 import com.intercorp.retail.reto.clientes.service.ClienteService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
@@ -22,21 +25,31 @@ public class ClienteController {
 	@Autowired
 	private ClienteService clienteService;
 
+	@ApiOperation(value = "Obtiene el listado de clientes", 
+		produces = MediaType.APPLICATION_JSON_UTF8_VALUE, 
+		consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, 
+		response = Cliente.class, 
+		httpMethod = "GET")
 	@GetMapping("/listclientes")
 	public List<Cliente> getAllClientes() {
 		return clienteService.findAll();
 	}
-	
+
+	@ApiOperation(value = "Registra un cliente", 
+		produces = MediaType.APPLICATION_JSON_UTF8_VALUE, 
+		consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, 
+		response = Cliente.class, 
+		httpMethod = "POST")
 	@PostMapping("/creacliente")
 	public Cliente createCliente(@RequestBody Cliente cliente) {
 		return clienteService.createCliente(cliente);
 	}
-	
+
 	@GetMapping("/kpideclientes")
 	public KpiCliente getKpiClientes() {
 		return clienteService.getKpiClientes();
 	}
-	
+
 	@DeleteMapping("/eliminarcliente/{id}")
 	public void deleteCliente(@PathVariable String id) {
 		clienteService.deleteCliente(id);
